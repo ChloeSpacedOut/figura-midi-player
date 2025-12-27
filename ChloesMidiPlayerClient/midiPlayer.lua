59ad79757a0b1eb2d5f45215a2626e6a29bec14e
ChloeSpacedOut <chloespacedout@gmail.com>
2025-12-27 17:20:36 +1300

    basic pinging

--- ChloesMidiPlayerClient/midiPlayer.lua
+++ ChloesMidiPlayerClient/midiPlayer.lua
@@ -2,51 +2,73 @@ local midiPlayer = {
     page = action_wheel:newPage("midiPlayerPage"),
     returnPage = nil,
     directory = "ChloesMidiPlayer",
-    hasFetchedMidis = false,
+    hasMadeInstance = false,
     midiAPI = world.avatarVars()["b0e11a12-eada-4f28-bb70-eb8903219fe5"],
     instance = nil,
     songs = {},
+    songIndex = {},
+    pingQueue = {},
     selectedSong = 1,
-    pageSize = 20
+    pageSize = 20,
+    pingSize = 200
 }
 
 local actions = {}
 
-
-function midiPlayer:addMidiPlayer(page)
-    midiPlayer.returnPage = page
-    actions.midiPlayer = page:newAction()
-        :setTitle("Midi Player\nERROR: Midi player avatar is not loaded")
-end
-
-local function fast_read_byte_array(path)
-    local stream = file:openReadStream(path)
-    local future = stream:readAsync()
-    repeat until future:isDone()
-    return future:getValue()--[[@as string]]
-end
-
-local function getMidiData(instance)
-    if not file:isDirectory(midiPlayer.directory) then
-        file:mkdir(midiPlayer.directory)
-        log('"'..midiPlayer.directory..'" folder has been created')
+function events.render()
+    if midiPlayer.instance then
+        midiPlayer.instance:updatePlayer()
     end
-    for k,fileName in pairs(file:list(midiPlayer.directory)) do
-        local path = midiPlayer.directory.."/"..fileName
-        local suffix = string.sub(fileName,-4,-1)
-        local name = string.sub(fileName,1,-5)
-        if suffix == ".mid" and (not instance.songs[name]) then
-            local midiData = fast_read_byte_array(path)
-            --log(readString(midiData))
-            instance:newSong(name,midiData)
+end
+
+function events.tick()
+    if midiPlayer.instance then
+        midiPlayer.instance:updateParser()
+    end
+end
+
+function events.tick()
+    if midiPlayer.midiAPI and (not midiPlayer.hasMadeInstance) then
+        local player = world.getEntity(avatar:getUUID())
+        midiPlayer.instance = midiPlayer.midiAPI.newInstance(player:getName(),player)
+        midiPlayer.hasMadeInstance = true
+        if host:isHost() then
+            actions.midiPlayer:setTitle("Midi Player")
+                :setOnLeftClick(function() action_wheel:setPage(midiPlayer.page) end)
         end
     end
 end
 
+function pings.newInstance()
+        
+end
+
+function pings.sendSong(ID,data)
+    if not midiPlayer.instance then return end
+    if not midiPlayer.instance.songs[ID] then
+        midiPlayer.instance:newSong(ID,data)
+    else
+        midiPlayer.instance.songs[ID].rawSong = midiPlayer.instance.songs[ID].rawSong .. data
+    end
+end
+
+function pings.updateSong(ID,action)
+    if not midiPlayer.instance then return end
+    if action == 1 then
+        midiPlayer.instance.songs[ID]:play()
+    elseif action == 2 then
+       midiPlayer.instance.songs[ID]:pause()
+    elseif action == 0 then
+        midiPlayer.instance.songs[ID]:stop()
+    end
+end
+
+if not host:isHost() then return end
+
 local function generateSongSelector()
     local songTitle = "song selector \n"
     local selectedPage = math.floor((midiPlayer.selectedSong - 1) / midiPlayer.pageSize)
-    for k,v in pairs(midiPlayer.songs) do
+    for k,v in pairs(midiPlayer.songIndex) do
         local currentPage = math.floor((k - 1) / midiPlayer.pageSize)
         if currentPage == selectedPage then
             if k == midiPlayer.selectedSong then
@@ -56,31 +78,9 @@ local function generateSongSelector()
             end
         end
     end
-    local lastPage = math.floor((#midiPlayer.songs - 1) / midiPlayer.pageSize)
+    local lastPage = math.floor((#midiPlayer.songIndex - 1) / midiPlayer.pageSize)
     songTitle = songTitle .. "§rpage " .. selectedPage + 1 .. " of " .. lastPage + 1
     actions.songs:setTitle(songTitle)
-end
-
-function events.MOUSE_PRESS(key,state,bitmast)
-    local actionWheelOpen = action_wheel:isEnabled()
-    local currentPage = action_wheel:getCurrentPage():getTitle()
-    local selectedAction = action_wheel:getSelected()
-    if actionWheelOpen and currentPage == "midiPlayerPage" and selectedAction == 3 then
-        if state == 1 then
-            if key == 0 then
-                local selectedSong = midiPlayer.instance.songs[midiPlayer.songs[midiPlayer.selectedSong]]
-                if selectedSong.state == "STOPPED" or selectedSong.state == "PAUSED" then
-                    selectedSong:play()
-                elseif selectedSong.state == "PLAYING" then
-                    selectedSong:pause()
-                end
-            elseif key == 1 then
-                if midiPlayer.instance.activeSong then
-                    midiPlayer.instance.songs[midiPlayer.instance.activeSong]:stop()
-                end
-            end
-        end
-    end
 end
 
 actions.back = midiPlayer.page:newAction()
@@ -97,38 +97,107 @@ actions.pingRate = midiPlayer.page:newAction()
 actions.songs = midiPlayer.page:newAction()
     :setTitle("songs")
     :setOnScroll(function(scroll)
-        midiPlayer.selectedSong = math.clamp(midiPlayer.selectedSong - scroll,1,#midiPlayer.songs)
+        midiPlayer.selectedSong = math.clamp(midiPlayer.selectedSong - scroll,1,#midiPlayer.songIndex)
         generateSongSelector()
     end)
 
-function events.tick()
-    if midiPlayer.midiAPI and (not midiPlayer.hasFetchedMidis) then
-        local player = world.getEntity(avatar:getUUID())
-        midiPlayer.instance = midiPlayer.midiAPI.newInstance(player:getName(),player)
-        getMidiData(midiPlayer.instance)
-        midiPlayer.hasFetchedMidis = true
-        actions.midiPlayer:setTitle("Midi Player")
-            :setOnLeftClick(function() action_wheel:setPage(midiPlayer.page) end)
-        
-        for k,v in pairs(midiPlayer.instance.songs) do
-            table.insert(midiPlayer.songs,k)
+midiPlayer.song = {}
+midiPlayer.song.__index = midiPlayer.song
+
+function midiPlayer.song:new(name,rawData)    
+    self = setmetatable({},midiPlayer.song)
+    self.ID = name
+    self.rawData = rawData
+    self.isPinged = false
+    self.totalChunks = math.floor(string.len(rawData) / midiPlayer.pingSize)
+    self.currentChunk = 0
+    return self
+end
+
+
+function midiPlayer:addMidiPlayer(page)
+    midiPlayer.returnPage = page
+    actions.midiPlayer = page:newAction()
+        :setTitle("Midi Player\nERROR: Midi player avatar is not loaded")
+end
+
+local function fast_read_byte_array(path)
+    local stream = file:openReadStream(path)
+    local future = stream:readAsync()
+    repeat until future:isDone()
+    return future:getValue()--[[@as string]]
+end
+
+local function getMidiData()
+    if not file:isDirectory(midiPlayer.directory) then
+        file:mkdir(midiPlayer.directory)
+        log('"'..midiPlayer.directory..'" folder has been created')
+    end
+    for k,fileName in pairs(file:list(midiPlayer.directory)) do
+        local path = midiPlayer.directory.."/"..fileName
+        local suffix = string.sub(fileName,-4,-1)
+        local name = string.sub(fileName,1,-5)
+        if suffix == ".mid" and (not midiPlayer.songs[name]) then
+            local midiData = fast_read_byte_array(path)
+            midiPlayer.songs[name] = midiPlayer.song:new(name,midiData)
+            table.insert(midiPlayer.songIndex,name)
         end
-        table.sort(midiPlayer.songs)
-        generateSongSelector()
     end
 end
 
-function events.render()
-    if midiPlayer.instance then
-        midiPlayer.instance:updatePlayer()
+getMidiData()
+table.sort(midiPlayer.songIndex)
+generateSongSelector()
+
+function events.MOUSE_PRESS(key,state,bitmast)
+    local actionWheelOpen = action_wheel:isEnabled()
+    local currentPage = action_wheel:getCurrentPage():getTitle()
+    local selectedAction = action_wheel:getSelected()
+    if actionWheelOpen and currentPage == "midiPlayerPage" and selectedAction == 3 then
+        if state == 1 then
+            local selectedSongLocal = midiPlayer.songs[midiPlayer.songIndex[midiPlayer.selectedSong]]
+            local selectedSongPinged = midiPlayer.instance.songs[midiPlayer.songIndex[midiPlayer.selectedSong]]
+            if key == 0 then
+                if not selectedSongLocal.isPinged then
+                    table.insert(midiPlayer.pingQueue,selectedSongLocal.ID)
+                else
+                    if selectedSongPinged.state == "STOPPED" or selectedSongPinged.state == "PAUSED" then
+                        pings.updateSong(selectedSongPinged.ID,1)
+                    elseif selectedSongPinged.state == "PLAYING" then
+                        pings.updateSong(selectedSongPinged.ID,2)
+                    end
+                end
+            elseif key == 1 then
+                if midiPlayer.instance.activeSong and selectedSongLocal.isPinged then
+                    pings.updateSong(midiPlayer.instance.songs[midiPlayer.instance.activeSong].ID,0)
+                end
+            end
+        end
     end
 end
 
+local clock = 0
 function events.tick()
-    if midiPlayer.instance then
-        midiPlayer.instance:updateParser()
+    clock = clock + 1
+    if not (clock % 20 == 0) then return end
+    local queuedSong = midiPlayer.pingQueue[1]
+    if queuedSong then
+        local currentChunk = midiPlayer.songs[queuedSong].currentChunk
+        local totalChunks = midiPlayer.songs[queuedSong].totalChunks
+        local rawData = midiPlayer.songs[queuedSong].rawData
+        local dataChunk = string.sub(rawData,currentChunk * midiPlayer.pingSize,((currentChunk + 1) * midiPlayer.pingSize) - 1)
+        if currentChunk == totalChunks then
+            midiPlayer.songs[queuedSong].isPinged = true
+            local newQueue = {}
+            for k = 2,#midiPlayer.pingQueue do
+                table.insert(newQueue,midiPlayer.pingQueue[k])
+            end
+            midiPlayer.pingQueue = newQueue
+        else
+            midiPlayer.songs[queuedSong].currentChunk = currentChunk + 1
+        end
+        pings.sendSong(queuedSong,dataChunk)
     end
 end
-
 
 return midiPlayer
--- ChloesMidiPlayerClient/script.lua
+++ ChloesMidiPlayerClient/script.lua
@@ -15,6 +15,8 @@ function events.tick()
     
 end
 
+if not host:isHost() then return end
+
 local midiPlayer = require("midiPlayer")
 local mainPage = action_wheel:newPage("mainPage")
 action_wheel:setPage(mainPage)
