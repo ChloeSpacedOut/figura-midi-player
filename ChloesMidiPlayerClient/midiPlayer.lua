9cd75f434288e322f4446afaca4ed70487bd745b
ChloeSpacedOut <chloespacedout@gmail.com>
2025-12-30 17:08:05 +1300

    Compressed Pinging

--- ChloesMidiPlayerClient/midiPlayer.lua
+++ ChloesMidiPlayerClient/midiPlayer.lua
@@ -55,6 +55,73 @@ function events.tick()
     end
 end
 
+local function readBits(buffer,numBytes)
+    local bufferPos = buffer:getPosition()
+    local bits = {}
+    for i = 0, (numBytes - 1) do
+        buffer:setPosition(bufferPos + (numBytes - 1) - i)
+        local currentVal = buffer:read()
+        for bit = 0,7 do
+            table.insert(bits,bit32.extract(currentVal,bit))
+        end
+    end
+    buffer:setPosition(bufferPos + numBytes)
+    return bits
+end
+
+local function variableLengthBitsToNum(bits)
+    local num = 0
+    local power = 0
+    for k,v in ipairs(bits) do
+        if not ((k) % 8 == 0) then
+            num = num + (bits[k] * (2 ^ power))
+            power = power + 1
+        else
+        end
+    end
+    return num
+end
+
+local function readVariableLengthInt(buffer,bufferLength)
+    local startPos = buffer:getPosition()
+    repeat
+        local val = buffer:read()
+        local signBit = bit32.extract(val,7)
+    until signBit == 0 or buffer:getPosition() == bufferLength
+    local endPos = buffer:getPosition()
+    buffer:setPosition(startPos)
+    local bits = readBits(buffer,endPos - startPos)
+    return variableLengthBitsToNum(bits)
+end
+
+local function decompressData(compressedData)
+    local buffer = data:createBuffer()
+    buffer:writeByteArray(compressedData)
+    buffer:setPosition(0)
+    local bufferLength = buffer:getLength()
+    local patternIndexEnd = readVariableLengthInt(buffer,bufferLength) + buffer:getPosition()
+    local patternIndex = {}
+    for i = 0,255 do
+        table.insert(patternIndex,string.char(i))
+    end
+    repeat
+        local index = readVariableLengthInt(buffer,bufferLength)
+        local numBytes = readVariableLengthInt(buffer,bufferLength)
+        local bytes = buffer:readByteArray(numBytes)
+        patternIndex[index] = bytes
+    until patternIndexEnd ==  buffer:getPosition() or bufferLength == buffer:getPosition()
+
+    local decompressedData = ""
+    repeat
+        local index = readVariableLengthInt(buffer,bufferLength)
+        if patternIndex[index] then
+            decompressedData = decompressedData .. patternIndex[index]
+        end
+    until bufferLength == buffer:getPosition()
+    buffer:close()
+    return decompressedData
+end
+
 function pings.sendSong(ID,currentChunk,isLastChunk,data)
     if not midiPlayer.instance then return end
     if not midiPlayer.instance.songs[ID] then
@@ -63,11 +130,11 @@ function pings.sendSong(ID,currentChunk,isLastChunk,data)
     end
     midiPlayer.instance.songs[ID].songChunks[currentChunk] = data
     if isLastChunk then
-        local rawSong = ""
+        local compressedSong = ""
         for _,chunk in ipairs(midiPlayer.instance.songs[ID].songChunks) do
-            rawSong = rawSong .. chunk
+            compressedSong = compressedSong .. chunk
         end
-        midiPlayer.instance.songs[ID].rawSong = rawSong
+        midiPlayer.instance.songs[ID].rawSong = decompressData(compressedSong)
     end
 end
 
@@ -163,7 +230,6 @@ function midiPlayer.song:new(name,rawData)
     self.ID = name
     self.rawData = rawData
     self.isPinged = false
-    self.totalChunks = math.floor(string.len(rawData) / midiPlayer.pingSize)
     self.currentChunk = 0
     self.nameLength = string.len(name)
     self.pingSize = midiPlayer.pingSize
@@ -205,6 +271,143 @@ getMidiData()
 table.sort(midiPlayer.songIndex)
 generateSongSelector()
 
+local function reverse(tab)
+    for i = 1, #tab/2, 1 do
+        tab[i], tab[#tab-i+1] = tab[#tab-i+1], tab[i]
+    end
+    return tab
+end
+
+
+local function toBits(num,bits)
+    local t = {} -- will contain the bits        
+    for b = bits, 1, -1 do
+        t[b] = math.fmod(num, 2)
+        num = math.floor((num - t[b]) / 2)
+    end
+    return reverse(t)
+end
+
+function numToVarLengthInt(num)
+    local numBits = math.max(1, select(2, math.frexp(num)))
+    --local numBytesBase = math.ceil(numBits / 8)
+    local numBytes = math.ceil(numBits / 7)
+    local bits = toBits(num,numBits)
+    for i = 1, (numBytes - 1) do
+        i = (numBytes - i) * 8
+        if i ~= 8 then 
+            table.insert(bits,i,1)
+        else   
+            table.insert(bits,i,0)
+        end
+    end
+    for i = 1, numBytes * 8 do
+        i = (numBytes * 8 ) - i + 1
+        if not bits[i] then
+            if i % 8 ~= 0 then 
+                bits[i] = 0
+            else
+                if numBytes ~= 1 then
+                    bits[i] = 1
+                else
+                    bits[i] = 0
+                end
+            end
+        end
+    end
+    local bitVal = 0
+    local val = ""
+    for i = 1, #bits do
+        bitVal = bitVal + bits[i] * 2 ^ ((i - 1) % 8)
+        if i % 8 == 0 then
+            val = val .. string.char(bitVal)
+            bitVal = 0
+        end
+    end
+    return string.reverse(val)
+end
+
+local function compressData(decompressedData)
+    local patternIndex = {}
+    local existingPatterns = {}
+    for i = 0,255 do
+        table.insert(patternIndex,string.char(i))
+        existingPatterns[string.char(i)] = #patternIndex
+    end
+    local buffer = data:createBuffer()
+    buffer:writeByteArray(decompressedData)
+    buffer:setPosition(0)
+    local bufferLength = buffer:getLength()
+    local readBytes = ""
+    repeat
+        local readBye = buffer:readByteArray(1)
+        local currentBytes = readBytes .. readBye
+        if not existingPatterns[currentBytes] then
+            table.insert(patternIndex,currentBytes)
+            existingPatterns[currentBytes] = #patternIndex
+            readBytes = ""
+        else
+            readBytes = currentBytes
+        end
+        if buffer:getPosition() == bufferLength then
+            table.insert(patternIndex,currentBytes)
+            existingPatterns[currentBytes] = #patternIndex
+        end
+    until buffer:getPosition() == bufferLength
+    buffer:setPosition(0)
+    
+    local patternCount = {}
+
+    for k,v in pairs(patternIndex) do
+        patternCount[v] = 0
+    end
+
+    local patternOrder = {}
+    readBytes = ""
+    repeat
+        local currentBytes = readBytes ..buffer:readByteArray(1)
+        if not existingPatterns[currentBytes] then
+            patternCount[readBytes] = patternCount[readBytes] + 1
+            table.insert(patternOrder,existingPatterns[readBytes])
+            local bufferPos = buffer:getPosition()
+            if bufferPos ~= bufferLength then
+                buffer:setPosition(bufferPos - 1)
+            end
+            readBytes = ""
+        else
+            readBytes = currentBytes
+        end
+        if buffer:getPosition() == bufferLength and readBytes ~= "" then
+            -- readBytes ~= "" may not account for the last byte
+            patternCount[currentBytes] = patternCount[currentBytes] + 1
+            table.insert(patternOrder,existingPatterns[currentBytes])
+        end
+    until buffer:getPosition() == bufferLength
+
+    for i = 0, 255 do
+        patternIndex[i] = nil
+    end
+
+    for pattern,count in pairs(patternCount) do
+        if count == 0 then
+            patternIndex[existingPatterns[pattern]] = nil
+        end
+    end
+
+    local compressedData = ""
+    local patternIndexString = ""
+    local patternOrderString = ""
+    for k,v in pairs(patternIndex) do
+        patternIndexString = patternIndexString .. numToVarLengthInt(k) .. numToVarLengthInt(string.len(v)) .. v
+    end
+    for k,v in pairs(patternOrder) do
+        patternOrderString = patternOrderString .. numToVarLengthInt(v)
+    end
+    compressedData = numToVarLengthInt(string.len(patternIndexString)) .. patternIndexString .. patternOrderString
+    buffer:close()
+    return compressedData
+end
+
 function events.MOUSE_PRESS(key,state,bitmast)
     local actionWheelOpen = action_wheel:isEnabled()
     local currentPage = action_wheel:getCurrentPage():getTitle()
@@ -216,6 +419,8 @@ function events.MOUSE_PRESS(key,state,bitmast)
             if key == 0 then
                 if not selectedSongLocal.isPinged then
                     table.insert(midiPlayer.pingQueue,selectedSongLocal.ID)
+                    selectedSongLocal.compressedData = compressData(selectedSongLocal.rawData)
+                    selectedSongLocal.totalChunks = math.floor(string.len(selectedSongLocal.compressedData) / midiPlayer.pingSize)
                 else
                     if selectedSongPinged.state == "STOPPED" or selectedSongPinged.state == "PAUSED" then
                         pings.updateSong(selectedSongPinged.ID,1)
@@ -249,8 +454,8 @@ function events.tick()
         local currentChunk = midiPlayer.songs[queuedSong].currentChunk
         local pingSize = midiPlayer.songs[queuedSong].pingSize - midiPlayer.songs[queuedSong].nameLength - 9
         local totalChunks = midiPlayer.songs[queuedSong].totalChunks
-        local rawData = midiPlayer.songs[queuedSong].rawData
-        local dataChunk = string.sub(rawData,currentChunk * pingSize,((currentChunk + 1) * pingSize) - 1)
+        local compressedData = midiPlayer.songs[queuedSong].compressedData
+        local dataChunk = string.sub(compressedData,currentChunk * pingSize,((currentChunk + 1) * pingSize) - 1)
         local isLastChunk = currentChunk == totalChunks
         if isLastChunk then
             midiPlayer.songs[queuedSong].isPinged = true
